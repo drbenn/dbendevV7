e56faf52237347a8793128c86ce7e3f0b128ecd9
drbenn <bennett.daniel@gmail.com>
2024-08-12 21:19:46 -0400

    contact page: basic for mvp

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,32 +1,35 @@
 const Contact = () => {
   return (
-    <main id="contact" className="w-full flex flex-row justify-center align-center my-[100px]">
-      <section className="w-3/5 text-center flex flex-row justify-around align-center">
-        <div>
-          <a href="mailto:bennett.daniel@gmail.com" target="_blank" rel="noopener noreferrer" 
-            className="text-blue-500 hover:underline">
-            Email
-          </a>
+    <main id="contact" className="w-full flex flex-row justify-center align-start mt-[100px]">
+      <div className="w-8/12 lg:w-6/12 flex flex-col justify-center align-start">
+        <div className="w-full flex flex-wrap flex-row justify-around align-center">
+          <div>
+            <a href="mailto:bennett.daniel@gmail.com" target="_blank" rel="noopener noreferrer">
+              Email
+            </a>
+          </div>
+          <div>
+            <a href="https://github.com/drbenn" target="_blank" rel="noopener noreferrer">
+              GitHub
+            </a>
+          </div>
+          <div>
+            <a href="https://www.linkedin.com/in/bennettdanielr/" target="_blank" rel="noopener noreferrer">
+              LinkedIn
+            </a>
+          </div>
+          <div>
+            <a href="https://leetcode.com/dbenn10/" target="_blank" rel="noopener noreferrer">
+              LeetCode
+            </a>
+          </div>
         </div>
-        <div>
-          <a href="https://github.com/drbenn" target="_blank" rel="noopener noreferrer" 
-            className="text-blue-500 hover:underline">
-            GitHub
-          </a>
+
+        <div className="text-center my-[75px] text-xs px-12">
+          Created by Dan Bennett using the most loved/hated Javascript library React
         </div>
-        <div>
-          <a href="https://www.linkedin.com/in/bennettdanielr/" target="_blank" rel="noopener noreferrer" 
-            className="text-blue-500 hover:underline">
-            LinkedIn
-          </a>
-        </div>
-        <div>
-          <a href="https://leetcode.com/dbenn10/" target="_blank" rel="noopener noreferrer" 
-            className="">
-            LeetCode
-          </a>
-        </div>
-      </section>
+
+      </div>
     </main>
   )
 }
