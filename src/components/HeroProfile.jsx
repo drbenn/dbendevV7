bfe525173be54d6b01fe64b8b7466347381e362b
drbenn <bennett.daniel@gmail.com>
2024-08-24 12:55:25 -0400

    ready for deploy

--- src/components/HeroProfile.jsx
+++ src/components/HeroProfile.jsx
@@ -1,8 +1,7 @@
-// const imageUrl = 'https://utfs.io/f/eb8d74c7-efce-46e3-9cc3-39b5d214f892-25qfy2.webp';
-import localImg from '../assets/img/p1.webp'
 import './styles/hero-profile.css';
 
 const HeroProfile = () => {
+  
   return (
     <main className='relative flex flex-col justify-start align-start select-none'>
       <div className="relative flex flex-row justify-center align-center">
@@ -15,7 +14,7 @@ const HeroProfile = () => {
         </section>
         <section className="hidden lg:flex flex-row justify-center align-center ml-4">
           <picture className="relative z-0 min-w-[350px] w-[29vw] max-w-[400px] h-[280px]">
-            <img src={localImg} alt="Dan Bennett Image" width="401" height="321" />
+            <img src='https://utfs.io/f/f8f60d00-9906-4f42-96c1-9b42a5adab8c-paeboe.webp' alt="Dan Bennett Image" width="401" height="321" />
           </picture>
           <div className="absolute z-0 min-w-[350px] w-[29vw] max-w-[400px] min-h-[290px] h-[23.5vw] max-h-[330px] img-overlay-gradient-t-b"></div>
           <div className="absolute z-0 min-w-[350px] w-[29vw] max-w-[400px] min-h-[290px] h-[23.5vw] max-h-[330px] img-overlay-gradient-l-r"></div>
