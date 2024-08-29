710ea8bd5d4aadc40d2ab6c5cd0d1e8b1f7b3fed
drbenn <bennett.daniel@gmail.com>
2024-08-29 14:24:33 -0400

    clean up navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,93 +1,20 @@
 import {
   Group,
-  Button,
-  UnstyledButton,
-  Text,
-  ThemeIcon,
-  Divider,
-  Center,
   Box,
   Burger,
   Drawer,
-  Collapse,
   ScrollArea,
   rem
-  // useMantineTheme,
 } from '@mantine/core';
-// import { MantineLogo } from '@mantinex/mantine-logo';
 import { useDisclosure } from '@mantine/hooks';
-import {
-  IconNotification,
-  IconCode,
-  IconBook,
-  IconChartPie3,
-  IconFingerprint,
-  IconCoin,
-  IconChevronDown,
-} from '@tabler/icons-react';
-// import classes from './HeaderMegaMenu.module.css';
 import './styles/navbar.css';
 import { useState, useEffect } from 'react';
 import logoImage from '../assets/icon/rocky.png';
 
-const mockdata = [
-  {
-    icon: IconCode,
-    title: 'Open source',
-    description: 'This Pokémon’s cry is very loud and distracting',
-  },
-  {
-    icon: IconCoin,
-    title: 'Free for everyone',
-    description: 'The fluid of Smeargle’s tail secretions changes',
-  },
-  {
-    icon: IconBook,
-    title: 'Documentation',
-    description: 'Yanma is capable of seeing 360 degrees without',
-  },
-  {
-    icon: IconFingerprint,
-    title: 'Security',
-    description: 'The shell’s rounded shape and the grooves on its.',
-  },
-  {
-    icon: IconChartPie3,
-    title: 'Analytics',
-    description: 'This Pokémon uses its flying ability to quickly chase',
-  },
-  {
-    icon: IconNotification,
-    title: 'Notifications',
-    description: 'Combusken battles with the intensely hot flames it spews',
-  },
-];
 
 
 const NavBar = () => {
   const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] = useDisclosure(false);
-  const [linksOpened, { toggle: toggleLinks }] = useDisclosure(false);
-  // const theme = useMantineTheme();
-
-  const links = mockdata.map((item) => (
-    <UnstyledButton className='' key={item.title}>
-      <Group wrap="nowrap" align="flex-start">
-        <ThemeIcon size={34} variant="default" radius="md">
-          <item.icon style={{ width: rem(22), height: rem(22) }} />
-        </ThemeIcon>
-        <div>
-          <Text size="sm" fw={500}>
-            {item.title}
-          </Text>
-          <Text size="xs" c="dimmed">
-            {item.description}
-          </Text>
-        </div>
-      </Group>
-    </UnstyledButton>
-  ));
-
-
   const [scrollY, setScrollY] = useState(0);
   const bgClassName = scrollY <= 15 ? 'bg-top' : 'bg-body';
 
